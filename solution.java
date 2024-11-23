e6556e7c10a865f1c504d0bd402c263bb6e93027
Kooreis <kooreis.official@gmail.com>
2024-11-22 23:27:16 -0600

    In this first update, the basic structure of the Java console application is set up. It includes the import statement for the Scanner class, the main class declaration, and the main method. Inside the main method, a new Scanner object is created to read user input from the console. The user is then prompted to enter a digit string.

--- solution.java
+++ solution.java
@@ -1,6 +1,3 @@
-Here is a Java console application that solves the problem:
-
-```java
 import java.util.Scanner;
 
 public class Main {
@@ -8,30 +5,5 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a digit string:");
         String digits = scanner.nextLine();
-        System.out.println("Number of ways to decode: " + numDecodings(digits));
-    }
-
-    public static int numDecodings(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-        int n = s.length();
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = s.charAt(0) != '0' ? 1 : 0;
-        for (int i = 2; i <= n; i++) {
-            int first = Integer.valueOf(s.substring(i - 1, i));
-            int second = Integer.valueOf(s.substring(i - 2, i));
-            if (first >= 1 && first <= 9) {
-                dp[i] += dp[i - 1];
-            }
-            if (second >= 10 && second <= 26) {
-                dp[i] += dp[i - 2];
-            }
-        }
-        return dp[n];
     }
 }
-```
-
-This program reads a digit string from the console, then calculates and prints the number of ways to decode the string. The decoding is based on the assumption that each digit or pair of digits can represent a letter (1-26).
