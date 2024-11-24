29d8b1354a6e103cfb39809344bc9253c2ced655
Kooreis <kooreis.official@gmail.com>
2024-11-23 23:27:16 -0600

    In the second update, a call to the numDecodings method is added, which is responsible for calculating the number of ways to decode the digit string entered by the user. The result is then printed to the console.

--- solution.java
+++ solution.java
@@ -1,9 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a digit string:");
         String digits = scanner.nextLine();
+        System.out.println("Number of ways to decode: " + numDecodings(digits));
     }
 }
